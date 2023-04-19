db97d5cdb081cc73719a4b4a5cf70eb7e0c68611
vinit <vinit@DESKTOP-NN757KN>
2023-04-19 13:38:45 -0300

    Adicionar arquivos de projeto.

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, reserva;
+            string[] nomesTrabalhadores;
+
+            Console.WriteLine("Qual quantidade de trabalhadores você deseja cadastrar?");
+            i = Convert.ToInt32(Console.ReadLine());
+            nomesTrabalhadores = new string [i];
+            reserva = i;
+
+            for (i = 0; i < reserva; i++)
+            {
+                Console.WriteLine($"Digite o nome do {i+1}º trabalhador:");
+                nomesTrabalhadores[i] = Console.ReadLine();
+            }
+            Console.Clear();
+            for (i = 0; i < reserva; i++)
+            {
+                Console.WriteLine($"O nome do {i+1}º trabalhador é: {nomesTrabalhadores[i]}");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
